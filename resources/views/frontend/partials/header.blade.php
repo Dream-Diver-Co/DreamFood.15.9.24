b505b4d6e0fe52f1bf4cf633d640d7936ef9f423
Sazal-Abdullah <sazalahamed61@gmail.com>
2024-09-18 13:02:37 +0600

    18

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,9 +1,13 @@
-   <!-- Navbar start -->
-   <div class="container-fluid nav-bar bg-white">
+<!-- Navbar start -->
+<div class="container-fluid nav-bar bg-white">
     <div class="container">
         <nav class="navbar navbar-light navbar-expand-lg">
             <a href="index.html" class="navbar-brand">
-                <h1 class="text-primary fw-bold mb-0">Dream<span class="text-dark">Foods</span> </h1>
+                <h1 class="text-primary fw-bold mb-0">Dream<span class="text-dark">Foods</span></h1>
+            </a>
+            <a href="{{ route('cart.show')}}" class="navbar-toggler py-2 px-3">
+                <i class="fa fa-shopping-cart mr-1"></i>
+                    <span class="">20</span>
             </a>
             <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars text-primary"></span>
@@ -18,9 +22,78 @@
                     <a href="chef.html" class="nav-item nav-link">Chef-At-Home</a>
                     <a href="contact.html" class="nav-item nav-link">Contact</a>
 
+                    <!-- Pages Dropdown -->
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <div class="dropdown-menu m-0">
+                            <a href="page1.html" class="dropdown-item">Page 1</a>
+                            <a href="page2.html" class="dropdown-item">Page 2</a>
+                            <a href="page3.html" class="dropdown-item">Page 3</a>
+                        </div>
+                    </div>
                 </div>
-                <button class="btn-search btn btn-primary btn-md-square me-4 rounded-circle d-none d-lg-inline-flex" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search"></i></button>
-                <a href="booking.html" class="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill">Book Now</a>
+
+                <!-- My Account Dropdown -->
+                <div class="btn-group nav-item me-4">
+                    <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-bs-toggle="dropdown">
+                        My Account
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right cart-dropdown-wrap cart-dropdown-hm2 account-dropdown">
+                        <ul class="list-unstyled p-2">
+                            @if(auth()->check())
+                                @if(auth()->user()->role === 'user')
+                                    <li>
+                                        <a href="{{ route('dashboard') }}" target="_blank"><i class="fi fi-rs-user mr-10"></i>Dashboard</a>
+                                    </li>
+                                @elseif(auth()->user()->role === 'admin')
+                                    <li>
+                                        <a href="{{ route('admin.dashboard') }}" target="_blank"><i class="fi fi-rs-user mr-10"></i>Dashboard</a>
+                                    </li>
+                                @elseif(auth()->user()->role === 'vendor')
+                                    <li>
+                                        <a href="{{ route('vendor.dashboard') }}" target="_blank"><i class="fi fi-rs-user mr-10"></i>Dashboard</a>
+                                    </li>
+                                @endif
+                                <li>
+                                    <a href="#"><i class="fi fi-rs-location-alt mr-10"></i>Order Tracking</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fi fi-rs-label mr-10"></i>My Voucher</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fi fi-rs-heart mr-10"></i>My Wishlist</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fi fi-rs-settings-sliders mr-10"></i>Setting</a>
+                                </li>
+                                <li>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <x-dropdown-link :href="route('logout')"
+                                                onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                            <i class="fi fi-rs-sign-out mr-10"></i>
+                                            {{ __('Log Out') }}
+                                        </x-dropdown-link>
+                                    </form>
+                                </li>
+                            @else
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('register') }}"><i class="fi fi-rs-user-add mr-10"></i>Create an Account</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('login') }}"><i class="fi fi-rs-sign-in mr-10"></i>Sign In</a>
+                                </li>
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Cart -->
+                <a href="{{ route('cart.show') }}" class="btn-search btn btn-primary btn-md-square me-4 rounded-circle d-none d-lg-inline-flex">
+                    <i class="fa fa-shopping-cart mr-1"></i>
+                    <span class="cart-count">20</span>
+                </a>
             </div>
         </nav>
     </div>
